350f4bc39fc3d91ab3fc9966a14b855f57192712
ShaQay3Q <shaghay3gh@gmail.com>
2024-10-08 14:41:31 +0200

    re-doing the dropdown menu for actions

---
+++ src/components/DropDownAction.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import * as React from "react";
+import { ChevronDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuLabel,
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function DropdownMenuRadioGroupDemo() {
+	const [category, setCategory] = React.useState("food");
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant='outline'>
+					{category.charAt(0).toUpperCase() + category.slice(1)}
+					<ChevronDown className='ml-2 h-4 w-4' />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className='w-56'>
+				<DropdownMenuLabel>Select Category</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuRadioGroup
+					value={category}
+					onValueChange={setCategory}
+				>
+					<DropdownMenuRadioItem value='food'>Food</DropdownMenuRadioItem>
+					<DropdownMenuRadioItem value='medicine'>
+						Medicine
+					</DropdownMenuRadioItem>
+				</DropdownMenuRadioGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
