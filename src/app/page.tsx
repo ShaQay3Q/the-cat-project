97126288a7d6a10d838a0aff3a1ad9d1ae6ab025
ShaQay3Q <shaghay3gh@gmail.com>
2024-11-13 22:09:27 +0100

    adding more of the project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import CatCard from "@/components/CatCard";
+import QRCodeGenerator from "@/components/QRCodeGenerator";
+
+const userId = "userId0456"
 export default function Home() {
 	return (
 		<div>
 			<CatCard />
+			<QRCodeGenerator userId={userId} />
 		</div>
 	);
 }
