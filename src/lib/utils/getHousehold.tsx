894f74750ac0cdde98630e6062b9259e887c52ce
ShaQay3Q <shaghay3gh@gmail.com>
2024-11-19 12:03:56 +0100

    again changes on getHousehold.tsx

--- src/lib/utils/getHousehold.tsx
+++ src/lib/utils/getHousehold.tsx
@@ -1,27 +1,29 @@
-// import { prisma } from "../db";
+import { prisma } from "../db";
 
-// export async function getHouseholdByEmail(householdEmail: string) {
-// 	const household = await prisma.household.findUnique({
-// 		where: { email: householdEmail },
-// 		select: {
-// 			id: true,
-// 			email: true,
-// 			// cats: true,
-// 			cats: {
-// 				select: {
-// 					id: true,
-// 					name: true,
-// 					image: true,
-// 					birthday: true,
-// 					healthStatus: true,
-// 					actions: true, // Include actions
-// 					schedules: true, // Include schedules if needed
-// 				},
-// 			},
-// 		},
-// 	});
+export async function getHouseholdByEmail(householdEmail: string) {
+	return;
 
-// 	return household;
-// }
+	// const household = await prisma.household.findUnique({
+	// 	where: { email: householdEmail },
+	// 	select: {
+	// 		id: true,
+	// 		email: true,
+	// 		// cats: true,
+	// 		cats: {
+	// 			select: {
+	// 				id: true,
+	// 				name: true,
+	// 				image: true,
+	// 				birthday: true,
+	// 				healthStatus: true,
+	// 				actions: true, // Include actions
+	// 				schedules: true, // Include schedules if needed
+	// 			},
+	// 		},
+	// 	},
+	// });
 
-// export type Household = Awaited<ReturnType<typeof getHouseholdByEmail>>;
+	// return household;
+}
+
+export type Household = Awaited<ReturnType<typeof getHouseholdByEmail>>;
