b83f9b574e5ac3a17a826d4546dbc57f8d2693e7
ShaQay3Q <shaghay3gh@gmail.com>
2024-11-28 19:39:59 +0100

    change in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,25 +12,30 @@ export default function Login() {
 		<div className='h-screen flex flex-col items-center bg-white'>
 			<div>
 				<Image
-					className='flex-1 object-cover object-left max-w-[400px] max-h-[400px]'
+					className='flex-1 object-cover object-left max-w-[300px] max-h-[300px]'
 					src='/cat.jpg' // Ensure to start with `/` for Next.js
 					alt='Drawing of a cat'
 					layout='responsive' // or `intrinsic`, `fixed`, or `fill` depending on your needs
-					width={400}
-					height={400}
+					width={300}
+					height={300}
 				/>
 			</div>
 			<div className='flex flex-1 px-5 flex-col items-center justify-evenly w-full'>
 				<div className='text-center'>
 					<h1 className='text-3xl font-semibold mb-2'>Hi there!</h1>
-					<h2 className='text-sm text-muted-foreground'>
-						Give a care to cats? <br /> Track there food and m here then!
+					<h2 className='text-sm text-muted-foreground mb-2'>
+						The Cat Prject <br /> Track your cat&#39;s food and medication.
 					</h2>
 				</div>
-				<div className='flex flex-col gap-6 items-center w-full'>
+				<div className='flex flex-col gap-5 items-center w-full'>
 					<div className='flex flex-col w-full gap-3'>
 						<Input placeholder='your@mail.com' />
+						<Input placeholder='password' />
+
 						<Button>Sign in with email</Button>
+						<Button className='bg-red-500'>
+							Don&#39;t have an account yet!
+						</Button>
 					</div>
 					<div className='w-full flex gap-3 items-center'>
 						<div className='h-px bg-muted-foreground/20 flex-1'></div>
