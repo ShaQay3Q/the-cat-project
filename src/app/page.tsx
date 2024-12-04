02c1a25d3f2a0258d25555e94eb2e5aad7909179
ShaQay3Q <shaghay3gh@gmail.com>
2024-12-04 22:27:05 +0100

    cat form is shaping up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,108 @@
-// import SubCadCard from "@/components/SubCadCard";
-// import { getHouseholdById } from "@/lib/utils/getHousehold";
-// import { ActionType } from "@prisma/client";
+"use client";
 
-export default async function Home() {
-	return <h1>Main Page</h1>;
-	// const cat = await getHouseholdById(1);
-	// if (!cat) {
-	// 	return;
-	// }
-	// if (cat) {
-	// 	return (
-	// 		<div className='flex flex-col p-6 gap-4 justify-start items-center align-top bg-slate-900 h-screen w-screen box-border'>
-	// 			<h1 className='text-neutral-50 font-extrabold leading-4 font'>
-	// 				Main Page
-	// 			</h1>
-	// 			<SubCadCard
-	// 				oneCat={cat}
-	// 				actiontype={"Food"}
-	// 			/>
-	// 		</div>
-	// 	);
-	// }
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+// import * as ReactHookForm from "react-hook-form";
+// const { useForm } = ReactHookForm;
+
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+// const FormSchema = z.object({
+// 	email: z.string().email({ message: "invalid email" }),
+// 	password: z.string(),
+// });
+
+// type FormFileds = {
+// 	email: string;
+// 	password: string;
+// };
+
+// Updated schema
+const FormSchema = z.object({
+	email: z.string().email({
+		message: "Ener a valid email",
+	}),
+	password: z.string().min(8, {
+		message: "minimum 6 character",
+	}),
+});
+
+export default function Home() {
+	const form = useForm<z.infer<typeof FormSchema>>({
+		resolver: zodResolver(FormSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+	// const onSubmit: SubmitHandler<FormFileds> = (data) => {
+	// 	console.log(data);
+	// };
+
+	// Submit handler
+	function onSubmit(data: z.infer<typeof FormSchema>) {
+		console.log(data);
+	}
+
+	return (
+		<div className='m-5'>
+			<h1>Main Page</h1>
+			<Form {...form}>
+				<form
+					className='test flex flex-col gap-4'
+					onSubmit={form.handleSubmit(onSubmit)}
+				>
+					{/* Email Field */}
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>e-mail</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='Enter your email'
+										className='rounded-md border border-gray-300 p-2'
+										{...field}
+									/>
+								</FormControl>
+								{/* <FormMessage /> */}
+							</FormItem>
+						)}
+					/>
+
+					{/* Email Field */}
+					<FormField
+						control={form.control}
+						name='password'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Password</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='Enter your password'
+										className='rounded-md border border-gray-300 p-2'
+										{...field}
+									/>
+								</FormControl>
+								{/* <FormMessage /> */}
+							</FormItem>
+						)}
+					/>
+					<Button type='submit'>Submit</Button>
+				</form>
+			</Form>
+		</div>
+	);
 }
